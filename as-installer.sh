5a2efee02c391f663574ec5d553f740e24af0757
NicoVarg99 <nicolasalsotto@gmail.com>
2015-12-13 21:46:24 +0100

    Update as-installer.sh

--- as-installer.sh
+++ as-installer.sh
@@ -6,6 +6,8 @@ if [ $(id -u) -ne 0 ]; then
 	exit 1
 fi
 
+mkdir /opt/anonsurf
+
 # Download anonsurf in /usr/bin
 wget --https-only https://raw.githubusercontent.com/EclipseSpark/anonsurf/master/anonsurf.sh -O /usr/bin/anonsurf
 chmod +x /usr/bin/anonsurf
@@ -14,21 +16,46 @@ chmod +x /usr/bin/anonsurf
 apt-get -y -qq install tor
 
 # Configure tor
-echo "VirtualAddrNetwork 10.192.0.0/10" >> /etc/tor/torrc
-echo "AutomapHostsOnResolve 1" >> /etc/tor/torrc
-echo "TransPort 9040" >> /etc/tor/torrc
-echo "SocksPort 9050" >> /etc/tor/torrca
-echo "DNSPort 53" >> /etc/tor/torrc
-echo "RunAsDaemon 1" >> /etc/tor/torrc
+f=/etc/tor/torrc
+echo "VirtualAddrNetwork 10.192.0.0/10" >> $f
+echo "AutomapHostsOnResolve 1" >> $f
+echo "TransPort 9040" >> $f
+echo "SocksPort 9050" >> $f
+echo "DNSPort 53" >> $f
+echo "RunAsDaemon 1" >> $f
 
-#TODO creare i .desktop e renderli eseguibili
+# Create and make executable the shortcuts
+wget http://bssec.altervista.org/images/as/anonstart.png -O /opt/anonsurf/anonstart.png
+wget http://bssec.altervista.org/images/as/anonstop.png -O /opt/anonsurf/anonstop.png
 
+f=/opt/anonsurf/as-start.desktop
+echo [Desktop Entry] >> $f
+echo Version=1.6 >> $f
+echo Name=AnonStart >> $f
+echo Exec=anonsurf start >> $f
+echo Icon=/opt/anonsurf/anonstart.png >> $f
+echo Terminal=false >> $f
+echo Type=Application >> $f
+echo Name=AnonStart >> $f
+chmod +x $f
 
-#TODO mettere in applicazioni i .desktop
-#cp /opt/anonsurf/as-start.desktop /usr/share/applications
-#cp /opt/anonsurf/as-stop.desktop /usr/share/applications
+f=/opt/anonsurf/as-stop.desktop
+echo [Desktop Entry] >> $f
+echo Version=1.6 >> $f
+echo Name=AnonStop >> $f
+echo Exec=anonsurf stop >> $f
+echo Icon=/opt/anonsurf/anonstop.png >> $f
+echo Terminal=false >> $f
+echo Type=Application >> $f
+echo Categories=Utility;Application; >> $f
+echo Name=AnonStop >> $f
+chmod +x $f
 
-#TODO mettere i desktop sulla scrivania
-#dir=$(xdg-user-dir DESKTOP)
-#cp /opt/anonsurf/as-start.desktop $dir
-#cp /opt/anonsurf/as-stop.desktop $dir
+# Put .desktop files in the applications directory
+cp /opt/anonsurf/as-start.desktop /usr/share/applications
+cp /opt/anonsurf/as-stop.desktop /usr/share/applications
+
+# Put .desktop files on the Desktop
+dir=$(xdg-user-dir DESKTOP)
+cp /opt/anonsurf/as-start.desktop $dir
+cp /opt/anonsurf/as-stop.desktop $dir
